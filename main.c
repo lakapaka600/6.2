b9ef19e9c9fe081deb8865f0a6a356cfb0066c40
Jaas <jasserfridhi08@gmail.com>
2025-02-02 22:36:52 +0100

    J

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    char squadre []={'N','I','A','L','J'};
+    char serie []={'A','A','A','A','B'};
+    int punti[]={53,48,45,38,10};
+    int i;
+    int dim=20;
+    printf("ELENCO SQUADRE SERIE A(FORZA INTER)");
+    printf("\n");
+    printf("SQUADRA  PUNTI");
+    printf("\n");
+    for(i=0;i<dim;i++) {
+        if(serie[i]=='A') {
+            printf("%c        %d\n",squadre[i],punti[i]);
+    }
+    }
+    return 0;
+}
